960594d812a45d3ea625d23f369f684a83aec7e4
EmmanuelPriest <emmanuelchukwujekwu87@gmail.com>
2023-04-13 07:02:09 -0700

    script that reads stdin line by line and computes metrics(via log parsing)

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,56 @@
 #!/usr/bin/python3
 '''Log Parsing'''
-import sys
-import signal
+from sys import stdin
 
 
-# Define a dictionary to store the number of lines for each status code
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+# Create dictionary to store the number of lines for each status code
+status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0}
 
-# Initialize the total file size and line count to zero
-total_size = 0
-line_count = 0
+# Initialize the total file size to zero
+file_size = 0
 
 
 def print_stats():
     '''Prints the current stats'''
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    print("File size: {}".format(file_size))
+    for key, val in sorted(status_codes.items()):
+        if val > 0:
+            print("{}: {}".format(key, val))
 
 
-def signal_handler(sig, frame):
-    '''Handles signal to print stats when script is interrupted'''
-    print_stats()
-    sys.exit(0)
+# def signal_handler(sig, frame):
+#    '''Handles signal to print stats when script is interrupted'''
+#    print_stats()
+#    sys.exit(0)
 
 # Register the signal handler for the SIGINT signal (CTRL+C)
-signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGINT, signal_handler)
+
 
 # Read lines from standard input and parse them
-for line in sys.stdin:
+if __name__ == "__main__":
     try:
-        parts = line.split(" ")
-        ip, _, _, _, _, _, _ = parts[:7]
-        code = int(parts[8])
-        size = int(parts[9])
-        total_size += size
-        if code in status_codes:
-            status_codes[code] += 1
-        line_count += 1
-        if line_count == 10:
-            print_stats()
-            line_count = 0
-    except (IndexError, ValueError):
-        # If the line is not in the specified format, skip it
-        continue
-
-# Print the final statistics when there is no more input
-print_stats()
+        for n, line in enumerate(stdin, 1):
+            try:
+                element = line.split()
+#        ip, _, _, _, _, _, _ = parts[:7]
+#        code = int(parts[8])
+                file_size += int(element[-1])
+#        total_size += size
+                if element[-2] in status_codes.keys():
+                    status_codes[element[-2]] += 1
+#        line_count += 1
+#        if line_count == 10:
+#            print_stats()
+#            line_count = 0
+            except (IndexError, ValueError):
+                # If the line is not in the specified format, skip it
+                pass
+            if not n % 10:
+                print_stats()
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    # Print the final statistics when there is no more input
+    print_stats()
